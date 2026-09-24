2a70c09f03c52fb030a35e8c550fc75d7403d3b3
Julián Fernández Drago <117131213+julianfdrago@users.noreply.github.com>
2026-09-23 21:14:11 -0300

    Add files via upload

---
+++ TP3_JPQL_facturacion_arca/src/main/java/com/facturacion/TP3_facturacion_arca_JPQL/src/main/java/com/facturacion/model/CondicionIva.java
@@ -0,0 +1,32 @@
+package com.facturacion.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "condicion_iva")
+public class CondicionIva extends AuditoriaApp {
+
+    @Column(nullable = false)
+    private int codigoAfip;
+
+    @Column(nullable = false)
+    private String denominacion;
+
+    public int getCodigoAfip() {
+        return codigoAfip;
+    }
+
+    public void setCodigoAfip(int codigoAfip) {
+        this.codigoAfip = codigoAfip;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+}
